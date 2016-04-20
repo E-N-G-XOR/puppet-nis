7a183faabaecbaa914f0795e8874a59afe684830
Luke Simmons <luke.simmons@vgregion.se>
2016-04-20 12:07:37 +0200

    Removing ypbind from service

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -4,13 +4,4 @@ class nis::server::install inherits nis {
     ensure => $nis::server_package_ensure,
   }
 
-  # Client service is used as ypbind needs to be notified
-  service { $nis::client_service:
-     ensure     => $nis::client_service_ensure,
-     enable     => $nis::client_service_enable,
-     hasrestart => $nis::client_service_hasrestart,
-     pattern    => $nis::nis_pattern,
-  }
-
-
 }
